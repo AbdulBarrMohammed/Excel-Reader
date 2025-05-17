7000c20f5d8a9c43647238e71f5167ef85a66563
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-05-17 19:24:13 -0400

    first commit

---
+++ ExcelReader/Model/Job.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcelReader.Model
+{
+    public class Job
+    {
+        public int Id { get; set; }
+    }
+}
